ff34f299e741a13c18bd9e710ffffc0f1108aa40
dio.muhammad <dio.muhammad@iongroup.com>
2020-07-30 23:24:27 +0700

    End of section 13

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,24 +2,40 @@ import axios, { AxiosResponse } from "axios";
 import { IActivity } from "../models/activity";
 import { history } from "../..";
 import { toast } from "react-toastify";
+import { IUser, IUserFormValues } from "../models/user";
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 
-axios.interceptors.response.use(undefined, error => {
-  if (error.message === 'Network Error' && !error.response) {
-    toast.error('Network error - make sure API is running!');
+axios.interceptors.request.use(
+  (config) => {
+    const token = window.localStorage.getItem("jwt");
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  const {status, data, config} = error.response;
+);
+
+axios.interceptors.response.use(undefined, (error) => {
+  if (error.message === "Network Error" && !error.response) {
+    toast.error("Network error - make sure API is running!");
+  }
+  const { status, data, config } = error.response;
   if (status === 404) {
-    history.push('/notfound');
+    history.push("/notfound");
   }
-  if (status === 400 && config.method === 'get' && data.errors.hasOwnProperty('id')) {
-    history.push('/notfound');
+  if (
+    status === 400 &&
+    config.method === "get" &&
+    data.errors.hasOwnProperty("id")
+  ) {
+    history.push("/notfound");
   }
-  if (status === 500){
-    toast.error('Server error - check the terminal for more info!');
+  if (status === 500) {
+    toast.error("Server error - check the terminal for more info!");
   }
-  throw error;
+  throw error.response;
 });
 
 const responseBody = (response: AxiosResponse) => response.data;
@@ -31,8 +47,10 @@ const sleep = (ms: number) => (response: AxiosResponse) =>
 
 const request = {
   get: (url: string) => axios.get(url).then(sleep(1000)).then(responseBody),
-  post: (url: string, body: {}) => axios.post(url, body).then(sleep(1000)).then(responseBody),
-  put: (url: string, body: {}) => axios.put(url, body).then(sleep(1000)).then(responseBody),
+  post: (url: string, body: {}) =>
+    axios.post(url, body).then(sleep(1000)).then(responseBody),
+  put: (url: string, body: {}) =>
+    axios.put(url, body).then(sleep(1000)).then(responseBody),
   del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody),
 };
 
@@ -45,6 +63,15 @@ const Activities = {
   delete: (id: string) => request.del(`/activities/${id}`),
 };
 
+const User = {
+  current: (): Promise<IUser> => request.get("/user"),
+  login: (user: IUserFormValues): Promise<IUser> =>
+    request.post("/user/login", user),
+  register: (user: IUserFormValues): Promise<IUser> =>
+    request.post("/user/register", user),
+};
+
 export default {
   Activities,
+  User,
 };
