4f96d54a71e087e4697e01acfd78d83d7481d2b5
dio.muhammad <dio.muhammad@iongroup.com>
2020-08-10 22:57:59 +0700

    End of section 17

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -3,6 +3,7 @@ import { IActivity } from "../models/activity";
 import { history } from "../..";
 import { toast } from "react-toastify";
 import { IUser, IUserFormValues } from "../models/user";
+import { IProfile, IPhoto } from "../models/profile";
 
 axios.defaults.baseURL = "http://localhost:5000/api";
 
@@ -52,6 +53,13 @@ const request = {
   put: (url: string, body: {}) =>
     axios.put(url, body).then(sleep(1000)).then(responseBody),
   del: (url: string) => axios.delete(url).then(sleep(1000)).then(responseBody),
+  postForm: (url: string, file: Blob) => {
+    let formData = new FormData();
+    formData.append('File', file);
+    return axios.post(url, formData, {
+      headers: {'Content-type': 'multipart/form-data'}
+    }).then(responseBody)
+  }
 };
 
 const Activities = {
@@ -73,7 +81,15 @@ const User = {
     request.post("/user/register", user),
 };
 
+const Profiles = {
+  get: (username: string): Promise<IProfile> => request.get(`/profiles/${username}`),
+  uploadPhoto: (photo: Blob): Promise<IPhoto> => request.postForm(`/photos`, photo),
+  setMainPhoto: (id: string) => request.post(`/photos/${id}/setMain`, {}),
+  deletePhoto: (id: string) => request.del(`/photos/${id}`),
+}
+
 export default {
   Activities,
   User,
+  Profiles
 };
